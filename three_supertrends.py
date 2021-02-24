078e538b93944d24eda158077f97c05dcc167378
luckyIITR <luckykushwaha56@gmail.com>
2021-02-24 09:02:32 +0530

    1 trade monitor

--- three_supertrends.py
+++ three_supertrends.py
@@ -135,11 +135,10 @@ tickers = ['GUJGASLTD.NS', 'ADANITRANS.NS', 'CONCOR.NS', 'M&M.NS',
        'VGUARD.NS', 'PNB.NS', 'BHEL.NS']
 tickers = ['PNB.NS']
 
-def main(p,q,ticker):
+def main():
     n = 50
-    global p_l
 
-
+    tickers = ['PNB.NS']
     # ticker = tickers[0]
     # tickers to track - recommended to use max movers from previous day
     capital = 3000  # position size
@@ -147,96 +146,76 @@ def main(p,q,ticker):
     st_dir = {}  # directory to store super trend status for each ticker
 
 
-    # for ticker in tickers:
-    st_dir[ticker] = ["None"]
-    print("starting passthrough for.....",ticker)
-    ohlc = get_intra_data(ticker,50)
-    ohlc["st1"] = supertrend(ohlc, p, q)
-    # ohlc["st1"] = supertrend(ohlc, p, m)
-    # ohlc["st2"] = supertrend(ohlc, q, n)
-    # ohlc["st3"] = supertrend(ohlc, r, o)
-    ohlc.dropna(inplace=True)
+    for ticker in tickers:
+        st_dir[ticker] = ["None"]
+        print("starting passthrough for.....",ticker)
+        ohlc = get_intra_data(ticker,50)
+        ohlc["st1"] = supertrend(ohlc, 5, 2)
+        # ohlc["st1"] = supertrend(ohlc, p, m)
+        # ohlc["st2"] = supertrend(ohlc, q, n)
+        # ohlc["st3"] = supertrend(ohlc, r, o)
+        ohlc.dropna(inplace=True)
 
-    # today[["st1","st2","st3"]].plot()
+        # today[["st1","st2","st3"]].plot()
 
-    pos = 0
-    percentchange = []
-    st_dir[ticker] = ["None"]
-    # today = get_only_today_data(ohlc, g)
-    today = ohlc
-    shifted = ohlc[['Close','st1']].shift(1)
-    for e in today.index:
-        if e == today.index[0]:
-            continue
-        if e.time() == dt.datetime(2020, 2, 5, 15, 25).time() or e.time() == dt.datetime(2020, 2, 5, 15, 20).time():
-            pos = 0
-            continue
-        # sl = sl_price(today, e)
-        st_dir_refresh(today, ticker, e, shifted)
-        # if st_dir[ticker] == ["green", "green", "green"]: break
-        quantity = int(capital / today.loc[e, "Close"])
-        if pos == 0 and e.time() != dt.datetime(2020, 2, 5, 15, 15).time():
-            if st_dir[ticker] == ["green"]:
-                pos = 1
-                bp = today.loc[e, "Close"]
-                # print(f"Buying at : {bp}  time : {e}")
+        pos = 0
+        percentchange = []
+        st_dir[ticker] = ["None"]
+        # today = get_only_today_data(ohlc, g)
+        today = ohlc
+        shifted = ohlc[['Close','st1']].shift(1)
+        for e in today.index:
+            if e == today.index[0]:
                 continue
-            if st_dir[ticker] == ["red"]:
-                pos = -1
+            if e.time() == dt.datetime(2020, 2, 5, 15, 25).time() or e.time() == dt.datetime(2020, 2, 5, 15, 20).time():
+                pos = 0
+                continue
+            # sl = sl_price(today, e)
+            st_dir_refresh(today, ticker, e, shifted)
+            # if st_dir[ticker] == ["green", "green", "green"]: break
+            quantity = int(capital / today.loc[e, "Close"])
+            if pos == 0 and e.time() != dt.datetime(2020, 2, 5, 15, 15).time():
+                if st_dir[ticker] == ["green"]:
+                    pos = 1
+                    bp = today.loc[e, "Close"]
+                    print(f"Buying at : {bp}  time : {e}")
+                    continue
+                if st_dir[ticker] == ["red"]:
+                    pos = -1
+                    sp = today.loc[e, "Close"]
+                    print(f"Selling at : {sp}  time : {e}")
+                    continue
+            if pos == 1 and st_dir[ticker] == ["red"] and e.time() != dt.datetime(2020, 2, 5, 15,15).time():
                 sp = today.loc[e, "Close"]
-                # print(f"Selling at : {sp}  time : {e}")
+                pos = -1
+                pc = (sp / bp - 1) * 100
+                print(f"Selling at : {sp}  time : {e} and pc : {pc}")
+                percentchange.append(pc)
                 continue
-        if pos == 1 and st_dir[ticker] == ["red"] and e.time() != dt.datetime(2020, 2, 5, 15,15).time():
-            sp = today.loc[e, "Close"]
-            pos = -1
-            pc = (sp / bp - 1) * 100
-            # print(f"Selling at : {sp}  time : {e} and pc : {pc}")
-            percentchange.append(pc)
-            continue
-        if pos == -1 and st_dir[ticker] == ["green"] and e.time() != dt.datetime(2020, 2, 5, 15,15).time():
-            bp = today.loc[e, "Close"]
-            pos = 1
-            pc = (1 - (bp / sp)) * 100
-            # print(f"Buying at : {bp}  time : {e} and pc : {pc}")
-            percentchange.append(pc)
-            continue
-        if pos == 1 and e.time() == dt.datetime(2020, 2, 5, 15, 15).time():
-            pos = 0
-            sp = today.loc[e, "Open"]
-            pc = (sp / bp - 1) * 100
-            # print(f"**Selling at : {sp}  time : {e} and pc : {pc}")
-            percentchange.append(pc)
-            continue
-        elif pos == -1 and e.time() == dt.datetime(2020, 2, 5, 15, 15).time():
-            pos = 0
-            bp = today.loc[e, "Open"]
-            pc = (1 - (bp / sp)) * 100
-            # print(f"***Buying at : {bp}  time : {e} and pc : {pc}")
-            percentchange.append(pc)
-            continue
+            if pos == -1 and st_dir[ticker] == ["green"] and e.time() != dt.datetime(2020, 2, 5, 15,15).time():
+                bp = today.loc[e, "Close"]
+                pos = 1
+                pc = (1 - (bp / sp)) * 100
+                print(f"Buying at : {bp}  time : {e} and pc : {pc}")
+                percentchange.append(pc)
+                continue
+            if pos == 1 and e.time() == dt.datetime(2020, 2, 5, 15, 15).time():
+                pos = 0
+                sp = today.loc[e, "Open"]
+                pc = (sp / bp - 1) * 100
+                print(f"**Selling at : {sp}  time : {e} and pc : {pc}")
+                percentchange.append(pc)
+                continue
+            elif pos == -1 and e.time() == dt.datetime(2020, 2, 5, 15, 15).time():
+                pos = 0
+                bp = today.loc[e, "Open"]
+                pc = (1 - (bp / sp)) * 100
+                print(f"***Buying at : {bp}  time : {e} and pc : {pc}")
+                percentchange.append(pc)
+                continue
 
-    print(np.array(percentchange).cumsum()[-1])
-    p_l.append(np.array(percentchange).cumsum()[-1])
-    # plt.plot(np.array(percentchange).cumsum())
-tic = []
-final = pd.DataFrame()
-for ticker in tickers:
-    p_l = []
-    p_val = []
-    q_val = []
-    tic.append(ticker)
-    for p in range(5,10):
-        for q in range(2,5):
-            p_val.append(p)
-            q_val.append(q)
-            st_dir = {}
-            print(p,q)
-            main(p,q,ticker)
-
-    overall = pd.DataFrame.from_dict({"p":p_val,"q":q_val,"p_l" : p_l}, orient='columns', dtype=None, columns=None)
-
-    # final = pd.DataFrame()
-    final = pd.concat([final,overall[overall.p_l == overall.p_l.max()]])
-
-    final.index = tic
-    final.to_csv("alert_data.csv")
+        print(np.array(percentchange).cumsum()[-1])
+        p_l.append(np.array(percentchange).cumsum()[-1])
+        # plt.plot(np.array(percentchange).cumsum())
+st_dir = {}
+main()
